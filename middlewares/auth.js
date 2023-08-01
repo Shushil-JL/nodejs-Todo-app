b88abbc733102fd5e765a95be3a1625510c6b083
Shushil <shushilchaudhary444@gmail.com>
2023-08-01 19:50:59 +0545

    code optimized for authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,30 +1,25 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.js";
+import errorHandler from "./error.js";
 
-export const isAuthenticated = async(req, res, next) => {
+export const isAuthenticated = async (req, res, next) => {
   // Check if the token cookie is present in the request
-  const{ token }= req.cookies;
+  const { token } = req.cookies;
   if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorized: Token missing",
-    });
+    return next(new errorHandler("Please, Login First", 401))
   }
 
   try {
     // Verify the token using your JWT secret
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-    
+
     // Attach the decoded user data to the request object for future use
     req.user = await User.findById(decodedData._id)
 
     // Proceed to the next middleware or route handler
     next();
   } catch (error) {
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorized: Invalid token",
-    });
+    return next(error)
   }
 };
 
